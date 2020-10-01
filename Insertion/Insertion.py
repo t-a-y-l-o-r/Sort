46d55a4a96b555ca8fa8327d18ecd078613e6ead
Taylor Cochran <taylorjcochran@hotmail.com>
2020-10-01 12:09:16 -0700

    Updated python with clean version of insertion sort

--- Insertion/Insertion.py
+++ Insertion/Insertion.py
@@ -7,20 +7,16 @@ def _insertion_sort(data):
     '''
     if not data:
         return data
-    cursor = 1
-    current_val = 0
-    still_sorting = cursor < len(data)
-    while still_sorting:
-        current_val = data[cursor]
-        for i, temp_val in enumerate(data[:cursor + 1]):
-            if current_val < temp_val:
-                data.pop(cursor)
-                data.insert(i, current_val)
-                break
-        cursor += 1
-        still_sorting = cursor < len(data)
+    for i in range(1, len(data)):
+        current = data[i]
+        previous_index = i - 1
+        sorting = (previous_index >= 0) and data[previous_index] > current
+        while sorting:
+            data[previous_index + 1] = data[previous_index]
+            previous_index -= 1
+            sorting = (previous_index >= 0) and data[previous_index] > current
+        data[previous_index + 1] = current
     return data
-
 
 
 if __name__ == "__main__":
